9e56d74f5d8268d90804f6e25e2aaea305d90850
Artem Danko <vpeta4141@gmail.com>
2023-03-18 15:56:44 +0200

    feat: init project

---
+++ .eslintrc.cjs
@@ -0,0 +1,45 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'import/prefer-default-export': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['off'],
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'jsx-a11y/anchor-is-valid': 'off',
+    'jsx-a11y/anchor-has-content': 'off',
+    'react/require-default-props': 'off',
+    'react/jsx-filename-extension': [0, { extensions: ['*.test{.ts,.tsx}'] }],
+  },
+};
---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button, Select } from '@shared';
+
+const App = () => {
+  return (
+    <div>
+      <Button></Button>
+      <Select></Select>
+    </div>
+  );
+};
+
+export default App;
